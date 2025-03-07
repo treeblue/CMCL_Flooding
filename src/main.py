4476e655093c048dccbfaf6072c5ad9a4d090677
Harrison <treeblue3@gmail.com>
2025-03-07 18:41:55 +0000

    added basic way to input station ID via the command line

--- src/main.py
+++ src/main.py
@@ -1,15 +1,11 @@
 #this uses Environment Agency flood and river level data from the real-time data API (Beta)
 from module import flooding
 
-
-other = "https://environment.data.gov.uk/flood-monitoring/id/floodAreas/122WAC953"
-trial = "https://environment.data.gov.uk/flood-monitoring/id/stations/1029TH/readings?latest.csv"
-
-
+# "1029TH"
+# "720763"
 
 if __name__ == "__main__":
-    df = flooding("1029TH")
-    # df = flooding("720763")
-    df.plot()
-
-    # plotter(df)
+    ID = input("Input Station ID:")
+    a = flooding(ID)
+    a.table(open=True)
+    a.plot()
